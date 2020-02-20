578c210e852df220bab07168f32ee1c76f410089
Kolja.vdt <kolja.vdt@users.noreply.github.com>
2020-02-20 17:13:07 +0100

    shortend the code and removed the config

--- config.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-
-welcomemsg = "Guess the number\nby Kolja von der Twer"
-max = 20
--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-import config
-from termcolor import colored
+from random import randint
+
 
 class NewGame:
-    print(config.welcomemsg)
-
     # initiating a new game, by setting all parameters you need and then starting a new iterative round
-    def __init__(self):
-        self.max = config.max
-        self.goal = config.randint(1, self.max)
+    def __init__(self, max: int):
+        '''
+        :param max: the maximum value you want your goal to be
+        '''
+        self.goal = randint(1, max)
         self.turns = 0
 
     def start(self):
+        print("Guess the number\nby Kolja von der Twer")
         self.new_round()
 
     # this method checks if the input of the user is equal to, less or higher than the guess.
-    # it also increases the number of turns every time you don't get the right number
+    # it also increases the number of turns every time call the method
     def new_round(self):
+        self.turns += 1
         guess = int(input('Please guess your number:\n'))
-        # this prevents the script from saying 0 turns
         if guess == self.goal:
-            self.turns += 1
-            print(colored('congratulations, you won the game in ' + str(self.turns) + ' turns!', 'yellow'))
+            print('congratulations, you won the game in ' + str(self.turns) + ' turns!', 'yellow')
         elif guess > self.goal:
             print('The number you entered was higher than the expected number')
-            self.turns += 1
             self.new_round()
-        elif guess < self.goal:
+        else:
             print('The number you entered was lower than the expected number')
-            self.turns += 1
             self.new_round()
 
 
